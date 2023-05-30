203bc00ac1d9472622bad69119f817a6dc793e50
Mohamad Khaled <mohamadknsult2@gmail.com>
2023-05-30 15:56:09 +0300

    30/5/2023

--- 3D Project/Assets/Scripts/CharacterMovement.cs
+++ 3D Project/Assets/Scripts/CharacterMovement.cs
@@ -5,6 +5,9 @@ public class CharacterMovement : MonoBehaviour
     CharacterController characterController;
     public float speed = 5;
     Transform cam;
+    float gravity = 10;
+    float verticalVelocity = 10;
+    public float jumpValue = 3;
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
@@ -13,18 +16,32 @@ public class CharacterMovement : MonoBehaviour
 
     private void Update()
     {
-        float horizontal = Input.GetAxis("Horziontal");
+        float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Vector3 moveDirection = new Vector3(horizontal, 0, vertical);
 
-        float angle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-        // Atan ظل الزاوية
-        // Rad2Deg : Radians-to-degrees درجة
-        //  cam.eulerAngles.y زاوية الكاميرا على محور y
-        // euler / eulerAngles مصطلحات رياضية تدل على زواياً لمجسم ثلاثي الأبعاد وفق معادلة معينة
+        if (characterController.isGrounded)
+        {
+            if (Input.GetAxis("Jump") > 0)
+                verticalVelocity = jumpValue;
+        }
+        else
+            verticalVelocity -= gravity * Time.deltaTime;
 
-        transform.rotation = Quaternion.Euler(0, angle, 0);
+        if (moveDirection.magnitude > 0.1)
+        {
+            // magnitude طول المتجه
+            float angle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            // Atan ظل الزاوية
+            // Rad2Deg : Radians-to-degrees درجة
+            //  cam.eulerAngles.y زاوية الكاميرا على محور y
+            // euler / eulerAngles مصطلحات رياضية تدل على زواياً لمجسم ثلاثي الأبعاد وفق معادلة معينة
+            transform.rotation = Quaternion.Euler(0, angle, 0);
 
-        characterController.Move(new Vector3(horizontal, 0, vertical) * Time.deltaTime * speed);
+        }
+        moveDirection = cam.TransformDirection(moveDirection);
+
+        moveDirection = new Vector3(moveDirection.x, verticalVelocity, moveDirection.z);
+        characterController.Move(moveDirection * Time.deltaTime * speed);
     }
 }
